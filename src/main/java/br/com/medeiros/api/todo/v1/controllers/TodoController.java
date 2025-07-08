a577ea496ad480fa07a9489617c339476f9abbe0
Matheus Medeiros <promatheusmedeiros@gmail.com>
2025-07-08 14:50:09 -0300

    chore: create TodoController with createTodo method

---
+++ src/main/java/br/com/medeiros/api/todo/v1/controllers/TodoController.java
@@ -0,0 +1,26 @@
+package br.com.medeiros.api.todo.v1.controllers;
+
+import br.com.medeiros.api.todo.v1.services.TodoService;
+import br.com.medeiros.api.todo.v1.data.RequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/todos/v1")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTodo(@RequestBody RequestDto requestDto){
+        var id = todoService.createTodo(requestDto.name(), requestDto.description());
+        return ResponseEntity.created(URI.create("/api/todos/v1/" + id.toString())).build();
+    }
+
+}
