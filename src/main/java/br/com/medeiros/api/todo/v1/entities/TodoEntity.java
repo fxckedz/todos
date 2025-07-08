b362ca5f0c6477ab2d6802744f830b5835456264
Matheus <promatheusmedeiros@gmail.com>
2025-07-07 22:35:10 -0300

    feat: create TodoEntity and TodoStatus(PENDING, COMPLETED)

---
+++ src/main/java/br/com/medeiros/api/todo/v1/entities/TodoEntity.java
@@ -0,0 +1,90 @@
+package br.com.medeiros.api.todo.v1.entities;
+
+import br.com.medeiros.api.todo.v1.enums.TodoStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "todos")
+public class TodoEntity {
+
+    public TodoEntity() {
+    }
+
+    public TodoEntity(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private TodoStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TodoStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TodoStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/br/com/medeiros/api/todo/v1/enums/TodoStatus.java
@@ -0,0 +1,6 @@
+package br.com.medeiros.api.todo.v1.enums;
+
+public enum TodoStatus {
+    PENDING,
+    COMPLETED
+}
