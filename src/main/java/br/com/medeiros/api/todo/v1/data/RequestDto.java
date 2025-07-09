96d3a0eea8ffecf5ef3fa00406ef2865d11fedb3
Matheus Medeiros <promatheusmedeiros@gmail.com>
2025-07-08 22:50:46 -0300

    chore: change deafult message error from ResquestDto

--- src/main/java/br/com/medeiros/api/todo/v1/data/RequestDto.java
+++ src/main/java/br/com/medeiros/api/todo/v1/data/RequestDto.java
@@ -3,7 +3,7 @@ package br.com.medeiros.api.todo.v1.data;
 import jakarta.validation.constraints.NotEmpty;
 
 public record RequestDto(
-        @NotEmpty
+        @NotEmpty(message = "can not be empty")
         String name,
         String description
 ) {}
