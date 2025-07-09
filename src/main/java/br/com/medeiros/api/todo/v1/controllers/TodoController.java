885aaa152d351ea28af689918b43f65a7e97aff4
Matheus <promatheusmedeiros@gmail.com>
2025-07-09 20:55:51 -0300

    feat: create findAllTodos controller

--- src/main/java/br/com/medeiros/api/todo/v1/controllers/TodoController.java
+++ src/main/java/br/com/medeiros/api/todo/v1/controllers/TodoController.java
@@ -2,11 +2,13 @@ package br.com.medeiros.api.todo.v1.controllers;
 
 import br.com.medeiros.api.todo.v1.services.TodoService;
 import br.com.medeiros.api.todo.v1.data.RequestDto;
+import br.com.medeiros.api.todo.v1.data.ResponseDto;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todos/v1")
@@ -22,6 +24,18 @@ public class TodoController {
     public ResponseEntity<Void> createTodo(@Valid @RequestBody RequestDto requestDto){
         var id = todoService.createTodo(requestDto.name(), requestDto.description());
         return ResponseEntity.created(URI.create("/api/todos/v1/" + id.toString())).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseDto>> findAllTodos(){
+
+        var todos = todoService.findAllTodos();
+
+        if(todos.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(todos);
     }
 
 }
